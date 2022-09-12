eb9189fdaeb9b71139af472277310886bdb0a36e
tangxuning <tangxuning@huoban.com>
2022-09-13 01:15:01 +0800

    ok

--- models/stockService.py
+++ models/stockService.py
@@ -9,6 +9,7 @@ import time
 import re
 import pandas as pd
 import requests
+import numpy as np
 
 
 class StockService:
@@ -49,30 +50,48 @@ class StockService:
         从csv文件中，获取所有持仓过的股票数据
         '''
         path = self.getPath()
-        all_files = glob.glob(os.path.join(path, "*今日成交.*.csv"))
+        all_files = glob.glob(os.path.join(path, "*历史成交-*.csv"))
         if not all_files:
             return '{}'
 
         all_df = []
         for f in all_files:
-            df = pd.read_csv(f, sep=',', usecols=[
-                             '市场', '证券代码', '证券名称'], dtype={'证券代码': str})
+            df = pd.read_csv(f, sep=',',  dtype={'证券代码': str})
             all_df.append(df)
 
-        merged_df = pd.concat(all_df, ignore_index=True, sort=True).drop_duplicates(
-            subset=['市场', '证券代码']).reset_index(drop=True)
+        merged_df = pd.concat(all_df, ignore_index=True, sort=True)
+        merged_df = merged_df[merged_df['业务名称'].isin(["证券买入", "证券卖出", "新股入账"])]
         # print(merged_df)
+
         merged_df['证券代码'] = merged_df['证券代码'].str.strip()
-        row_num = merged_df.shape[0]
+        merged_df['证券名称'] = merged_df['证券名称'].str.strip()
+
+        merged_df['交易金额'] = np.where(
+            merged_df['买卖方向'] == '买入', merged_df['交易金额'], -merged_df['交易金额'])
+        # merged_df['成交数量'] = np.where(
+        #    merged_df['买卖方向'] == '买入', merged_df['成交数量'], -merged_df['成交数量'])
+        res_df = merged_df.groupby(
+            ['证券代码', '市场'], as_index=False).sum()
+        # print(res_df)
+        merged_df = merged_df.drop(
+            columns=['买卖方向', '交易金额', '业务名称', '成交编号', '成交价格', '成交数量', '成交日期', '成交时间', '股东账号']).drop_duplicates(subset=['证券代码', '市场'], keep='first')
+
+        # res_df = pd.concat([res_df, merged_df], axis=1,
+        #                   join='inner', ignore_index=True)
+
+        res_df = pd.merge(res_df, merged_df,  on=[
+            '证券代码', '市场'])
+        # print(res_df)
+        row_num = res_df.shape[0]
         json_str = '{"code":200,"results":['
-        for index, row in merged_df.iterrows():
+        for index, row in res_df.iterrows():
             json_str = json_str + '{"key":'+str(index)+',"code":"'+row['证券代码']+'","name":"'\
-                + row['证券名称'] + '","market":"'+row['市场']+'"}'
+                + row['证券名称'] + '","market":"'+row['市场']+'","price":"' + \
+                str(row['交易金额'])+'","num":"'+str(row['成交数量'])+'"}'
             if index < row_num - 1:
                 json_str = json_str + ','
 
         json_str = json_str + ']}'
-        # print(json_str)
         return json_str
 
     def getHistory(self, stock_code, market):
@@ -80,7 +99,7 @@ class StockService:
         通过股票代码，市场，从csv列表文件中获取股票的历史交易数据
         '''
         path = self.getPath()
-        all_files = glob.glob(os.path.join(path, "*今日成交.*.csv"))
+        all_files = glob.glob(os.path.join(path, "*历史成交-*.csv"))
         if not all_files:
             return '{}'
 
@@ -88,7 +107,9 @@ class StockService:
         # return
         all_df = []
         for f in all_files:
-            df = pd.read_csv(f, sep=',', dtype={'证券代码': str, '股东账号': str})
+            df = pd.read_csv(f, sep=',', dtype={
+                             '证券代码': str, '股东账号': str, '成交日期': str})
+            '''
             file_name = f.split('/')[-1]
             date = file_name.split('.')
             YEARS = re.findall(r'\d{4}', date[1])
@@ -100,7 +121,7 @@ class StockService:
                 Y = YEARS[0]
                 df['成交日期'] = Y + '-' + date[2]+'-'+date[3]
             # print(Y)
-
+            '''
             all_df.append(df)
 
         merged_df = pd.concat(all_df, ignore_index=True, sort=True)
@@ -111,8 +132,11 @@ class StockService:
         response_body = '{"code":200,"results":['
         row_num = result_df.shape[0]
         for index, row in result_df.iterrows():
+            temp = list(row['成交日期'].strip())
+            temp.insert(4, '-')
+            temp.insert(7, '-')
             response_body = response_body + '{"code":"'+stock_code+'","date":"'\
-                + row['成交日期']+'","time":"'+row['成交时间'].strip()+'","sell_buy":"'+row['买卖方向']+'","price":' + \
+                + ''.join(temp)+'","time":"'+row['成交时间'].strip()+'","sell_buy":"'+row['买卖方向']+'","price":' + \
                 str(row['成交价格'])+',"num":'+str(row['成交数量'])+'}'
             if index < row_num - 1:
                 response_body = response_body + ','
